5edca00c14f3ba5878d306a3598092c125d1acb4
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2011-04-21 20:51:39 +0200

    Generate JavaDocs for development components

--- src/main/java/org/arachna/netweaver/javadoc/JavaDocBuilder.java
+++ src/main/java/org/arachna/netweaver/javadoc/JavaDocBuilder.java
@@ -2,50 +2,103 @@ package org.arachna.netweaver.javadoc;
 
 import hudson.Extension;
 import hudson.Launcher;
-import hudson.PluginFirstClassLoader;
-import hudson.PluginWrapper;
 import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Hudson;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collection;
+import java.util.HashSet;
 
 import net.sf.json.JSONObject;
 
+import org.apache.tools.ant.BuildException;
 import org.arachna.netweaver.dc.types.DevelopmentComponent;
+import org.arachna.netweaver.hudson.nwdi.DCWithJavaSourceAcceptingFilter;
 import org.arachna.netweaver.hudson.nwdi.NWDIBuild;
+import org.arachna.netweaver.hudson.nwdi.NWDIProject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
  * {@link Builder} for JavaDoc documentation for SAP NetWeaver development
  * components.
- * 
+ *
  * @author Dirk Weigenand
  */
 public class JavaDocBuilder extends Builder {
+
+    /**
+     * URLs to JavaDoc generated elsewhere that should be linked into the
+     * generated javadoc documentation.
+     */
+    private Collection<URL> links = new HashSet<URL>();
+
+    /**
+     * @return the links
+     */
+    public String getLinks() {
+        StringBuilder links = new StringBuilder();
+
+        for (URL link : this.links) {
+            links.append(link.toString()).append('\n');
+        }
+
+        return links.toString();
+    }
+
     // Fields in config.jelly must match the parameter names in the
     // "DataBoundConstructor"
     @DataBoundConstructor
-    public JavaDocBuilder() {
+    public JavaDocBuilder(String links) {
+        setLinks(links);
+    }
+
+    private void setLinks(String links) {
+        if (links != null) {
+            for (String link : links.split("\n\r")) {
+                String trimmedLink = link.trim();
+
+                if (trimmedLink.length() > 0) {
+                    try {
+                        this.links.add(new URL(trimmedLink));
+                    }
+                    catch (MalformedURLException mfue) {
+                        // FIXME: Should say something about malformed package
+                        // list urls
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public boolean perform(final AbstractBuild build, final Launcher launcher, final BuildListener listener) {
         final NWDIBuild nwdiBuild = (NWDIBuild)build;
-        final Collection<DevelopmentComponent> components = nwdiBuild.getAffectedDevelopmentComponents();
+        final JavaDocExecutor executor =
+            new JavaDocExecutor(nwdiBuild.getDevelopmentConfiguration(), nwdiBuild.getAntHelper(listener.getLogger()),
+                nwdiBuild.getDevelopmentComponentFactory(), this.links);
 
-        final PluginWrapper pluginWrapper = Hudson.getInstance().getPluginManager().getPlugin("NWDI-Checkstyle-Plugin");
-        final PluginFirstClassLoader pluginFirstClassLoader = (PluginFirstClassLoader)pluginWrapper.classLoader;
-        Thread.currentThread().setContextClassLoader(pluginFirstClassLoader);
+        for (final DevelopmentComponent component : nwdiBuild
+            .getAffectedDevelopmentComponents(new DCWithJavaSourceAcceptingFilter())) {
+            long start = System.currentTimeMillis();
 
-        final JavaDocExecutor executor = new JavaDocExecutor(nwdiBuild.getAntHelper(listener.getLogger()));
-
-        for (final DevelopmentComponent component : components) {
-            executor.execute(component);
+            try {
+                listener.getLogger().append(
+                    String.format("Running JavaDoc on %s:%s...", component.getVendor(), component.getName()));
+                executor.execute(component);
+            }
+            catch (BuildException be) {
+                listener.getLogger().append(
+                    String.format("\n%s: %s\n", be.getLocalizedMessage().trim(), component.getSourceFolders()));
+            }
+            finally {
+                listener.getLogger().append(
+                    String.format(" (%f sec.).\n", (System.currentTimeMillis() - start) / 1000f));
+            }
         }
 
         return true;
@@ -62,11 +115,6 @@ public class JavaDocBuilder extends Builder {
     /**
      * Descriptor for {@link JavaDocBuilder}. Used as a singleton. The class is
      * marked as public so that it can be accessed from views.
-     * 
-     * <p>
-     * See
-     * <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
      */
     @Extension
     // This indicates to Jenkins that this is an implementation of an extension
@@ -74,7 +122,7 @@ public class JavaDocBuilder extends Builder {
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
         @Override
         public boolean isApplicable(final Class<? extends AbstractProject> aClass) {
-            return NWDIBuild.class.equals(aClass);
+            return NWDIProject.class.equals(aClass);
         }
 
         /**
--- src/main/java/org/arachna/netweaver/javadoc/JavaDocExecutor.java
+++ src/main/java/org/arachna/netweaver/javadoc/JavaDocExecutor.java
@@ -5,53 +5,161 @@ package org.arachna.netweaver.javadoc;
 
 import hudson.model.Hudson;
 
+import java.io.File;
+import java.io.PrintStream;
 import java.net.InetSocketAddress;
+import java.net.URL;
+import java.util.Collection;
 import java.util.HashSet;
 
+import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.taskdefs.Javadoc;
+import org.apache.tools.ant.taskdefs.Javadoc.LinkArgument;
 import org.apache.tools.ant.types.FileSet;
 import org.arachna.ant.AntHelper;
+import org.arachna.ant.ExcludesFactory;
+import org.arachna.netweaver.dc.types.Compartment;
+import org.arachna.netweaver.dc.types.CompartmentState;
 import org.arachna.netweaver.dc.types.DevelopmentComponent;
+import org.arachna.netweaver.dc.types.DevelopmentComponentFactory;
+import org.arachna.netweaver.dc.types.DevelopmentConfiguration;
+import org.arachna.netweaver.dc.types.PublicPartReference;
+import org.arachna.netweaver.dctool.JdkHomeAlias;
 
 /**
  * Wrapper for the Ant JavaDoc task. Sets up the task and executes it.
- * 
+ *
  * @author Dirk Weigenand
  */
-public final class JavaDocExecutor {
+final class JavaDocExecutor {
     /**
      * Helper class for setting up an ant task with class path, source file sets
      * etc.
      */
     private final AntHelper antHelper;
 
+    /**
+     * The HTTP-Proxy to use iff available.
+     */
     private final InetSocketAddress proxy;
 
     /**
-     * 
-     * @param antHelper
+     * links to add to existing javadoc documentation
      */
-    JavaDocExecutor(final AntHelper antHelper) {
+    private final Collection<URL> links;
+
+    /**
+     * Registry for development components.
+     */
+    private final DevelopmentComponentFactory dcFactory;
+
+    private DevelopmentConfiguration developmentConfiguration;
+
+    /**
+     * Create an executor for executing the Javadoc ant task using the given ant
+     * helper object and links to related javadoc documentation.
+     *
+     * @param developmentConfiguration
+     *
+     * @param antHelper
+     *            helper for populating an ant task with source filesets and
+     *            class path for a given development component
+     * @param links
+     *            links to add to existing javadoc documentation
+     */
+    JavaDocExecutor(final DevelopmentConfiguration developmentConfiguration, final AntHelper antHelper,
+        final DevelopmentComponentFactory dcFactory, Collection<URL> links) {
+        this.developmentConfiguration = developmentConfiguration;
         this.antHelper = antHelper;
+        this.dcFactory = dcFactory;
+        this.links = links;
         proxy = (InetSocketAddress)Hudson.getInstance().proxy.createProxy().address();
     }
 
-    public void execute(final DevelopmentComponent component) {
-        final Javadoc task = new Javadoc();
-        final HashSet<String> excludes = new HashSet<String>();
+    JavaDocExecutor(final DevelopmentConfiguration developmentConfiguration, final AntHelper antHelper,
+        final DevelopmentComponentFactory dcFactory, Collection<URL> links, InetSocketAddress proxy) {
+        this.developmentConfiguration = developmentConfiguration;
+        this.antHelper = antHelper;
+        this.dcFactory = dcFactory;
+        this.links = links;
+        this.proxy = proxy;
+    }
 
-        for (final FileSet sources : antHelper.createSourceFileSets(component, excludes, excludes)) {
-            task.addFileset(sources);
+    /**
+     * Run the Ant Javadoc task for the given development component.
+     *
+     * @param component
+     *            development component to document with JavaDoc.
+     */
+    public void execute(final DevelopmentComponent component) {
+        File javaDocFolder = new File(this.getJavaDocFolder(component));
+
+        if (!javaDocFolder.exists() && !javaDocFolder.mkdirs()) {
+            throw new RuntimeException(String.format("Can't create %s!", this.getJavaDocFolder(component)));
         }
 
-        final Project project = new Project();
+        final Javadoc task = new Javadoc();
+
+        Project project = new Project();
+        task.setProject(project);
         task.setClasspath(antHelper.createClassPath(project, component));
         task.setEncoding("UTF-8");
         task.setUse(true);
         task.setAuthor(true);
+        task.setDestdir(javaDocFolder);
+
+        setLinks(task, component);
+
+        final HashSet<String> excludes = new HashSet<String>();
+        setFileSets(project, task, antHelper.createSourceFileSets(component, excludes, excludes));
 
         setProxyConfigurationParams(task);
+
+        task.setSource(getSourceVersion());
+        task.setHeader(String.format("Compartment %s<br/>Development Component %s:%s", component.getCompartment()
+            .getName(), component.getVendor(), component.getName()));
+
+        task.execute();
+    }
+
+    private String getSourceVersion() {
+        JdkHomeAlias alias = this.developmentConfiguration.getJdkHomeAlias();
+        String sourceVersion;
+        if (alias != null) {
+            sourceVersion = alias.getSourceVersion();
+        }
+        else {
+            String[] versionParts = System.getProperty("java.version").replace('_', '.').split("\\.");
+            sourceVersion = String.format("%s.%s", versionParts[0], versionParts[1]);
+        }
+
+        return sourceVersion;
+    }
+
+    private void setLinks(final Javadoc task, final DevelopmentComponent component) {
+        for (URL linkURL : this.links) {
+            LinkArgument link = task.createLink();
+            link.setPackagelistURL(linkURL);
+            link.setOffline(false);
+        }
+
+        for (PublicPartReference referencedDC : component.getUsedDevelopmentComponents()) {
+            DevelopmentComponent usedDC = this.dcFactory.get(referencedDC.getVendor(), referencedDC.getName());
+
+            if (usedDC != null && usedDC.getCompartment().isSourceState()) {
+                LinkArgument link = task.createLink();
+                link.setPackagelistLoc(new File(getOfflineJavaDocFolder(usedDC)));
+                link.setOffline(true);
+            }
+        }
+    }
+
+    private void setFileSets(Project project, Javadoc task, Collection<FileSet> sources) {
+        for (final FileSet source : sources) {
+            source.setProject(project);
+            task.addFileset(source);
+        }
     }
 
     private void setProxyConfigurationParams(final Javadoc task) {
@@ -64,5 +172,35 @@ public final class JavaDocExecutor {
                 task.setAdditionalparam(String.format("-J-Dhttp.proxyHost=%s", proxy.getHostName()));
             }
         }
+    }
+
+    private String getOfflineJavaDocFolder(DevelopmentComponent component) {
+        return String.format("%s/javadoc/%s~%s/package-list", this.antHelper.getPathToWorkspace(),
+            component.getVendor(), component.getName().replace('/', '~')).replace('/', File.separatorChar);
+    }
+
+    private String getJavaDocFolder(DevelopmentComponent component) {
+        return String.format("%s/javadoc/%s~%s", this.antHelper.getPathToWorkspace(), component.getVendor(),
+            component.getName().replace('/', '~')).replace('/', File.separatorChar);
+    }
+
+    public static final void main(String args[]) {
+        DevelopmentConfiguration developmentConfiguration = new DevelopmentConfiguration("PN3_Libs70_D");
+        Compartment compartment =
+            new Compartment("LIBJEE", CompartmentState.Source, "gisa.de", "Libraries für NW 7.0", "LIBJEE");
+        developmentConfiguration.add(compartment);
+        DevelopmentComponentFactory dcFactory = new DevelopmentComponentFactory();
+        DevelopmentComponent component = dcFactory.create("gisa.de", "lib/security/api/test/helper");
+        component.addSourceFolder("src/packages");
+        // component.addSourceFolder("test/packages");
+
+        compartment.add(component);
+        AntHelper antHelper =
+            new AntHelper("C:/tmp/hudson/jobs/libjee/workspace", dcFactory, new ExcludesFactory(), new PrintStream(
+                System.err));
+
+        JavaDocExecutor executor =
+            new JavaDocExecutor(developmentConfiguration, antHelper, dcFactory, new HashSet<URL>(), null);
+        executor.execute(component);
     }
 }
